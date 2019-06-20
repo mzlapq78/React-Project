a976a14ecbe0d86d7ceb96bbcd9d780e6874ac14
mzlapq78 <mzlapq78@163.com>
2019-06-20 23:48:40 +0800

    Create React Project

---
+++ src/components/Customer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App';
+
+class Customer extends React.Component {
+    render() {
+        <div>
+            <h2>金亨辰</h2>
+        </div>
+    }
+}
+
+
+export default Customer;
