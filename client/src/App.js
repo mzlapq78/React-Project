204591d074f2d74a45183adb55742fca46ab8ae7
mzlapq78 <mzlapq78@163.com>
2019-06-21 04:23:27 +0800

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import { async } from 'rxjs/internal/scheduler/async';
 
 const styles = theme => ({
   root: {
@@ -26,21 +27,25 @@ const customer ={
   'name': '金亨辰'
 }
 
-const customers =[
-{
-  'id': '2',
-  'image': 'https://placeimg.com/64/64/8',
-  'name': '郑彩媛'
-},
-{
-  'id': '3',
-  'image': 'https://placeimg.com/64/64/6',
-  'name': '金雪熙'
-}
-]
 
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+     .then(res => this.setState({customers: res}))
+     .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -52,7 +57,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c => { return ( <Customer key={c.id} name={c.name} image={c.image}/>);})}
+          {this.state.customers ? this.state.customers.map(c => { 
+            return ( <Customer key={c.id} name={c.name} image={c.image}/>);
+          }): ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'hello express'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': '2',
+            'image': 'https://placeimg.com/64/64/8',
+            'name': '郑彩媛'
+        },
+        {
+            'id': '3',
+            'image': 'https://placeimg.com/64/64/6',
+            'name': '金雪熙'
+        }
+    ]);
 
 });
 
